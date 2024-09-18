dc4d4efd3e8e4368ab573cd162819a4feb707159
labcabrera <lab.cabrera@gmail.com>
2024-09-18 13:29:38 +0200

    Changed port.

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 3001;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/rmu-core';
 
 app.use(express.json());
