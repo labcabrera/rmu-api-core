26b8fc4238f86a317f94fa1cf81966e7a6d861fc
labcabrera <lab.cabrera@gmail.com>
2024-09-18 10:49:31 +0200

    Initial version.

--- docker-run.sh
+++ docker-run.sh
@@ -8,6 +8,6 @@ docker rmi labcabrera/rmu-api-core:latest
 
 docker build -t labcabrera/rmu-api-core:latest .
 
-docker run -d -p 3003:3003 --network rmu-network --name rmu-api-core -h rmu-api-core -e MONGO_URI='mongodb://rmu-mongo:27017/rmu-core' -e PORT='3003' labcabrera/rmu-api-core:latest
+docker run -d -p 3001:3001 --network rmu-network --name rmu-api-core -h rmu-api-core -e MONGO_URI='mongodb://rmu-mongo:27017/rmu-core' -e PORT='3001' labcabrera/rmu-api-core:latest
 
 docker logs -f rmu-api-core
