3625d338cb7dba8d2d65cd442273f71fa8935e50
labcabrera <lab.cabrera@gmail.com>
2024-09-19 15:35:13 +0200

    Updated character sizes model.

--- src/routes/character-size-controller.js
+++ src/routes/character-size-controller.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const characterSizes = [
-    { 'id': 1, 'name': 'Minuscule', 'hitMultiplier': 0.25 },
-    { 'id': 2, 'name': 'Diminutive', 'hitMultiplier': 0.50 },
-    { 'id': 3, 'name': 'Tiny', 'hitMultiplier': 0.67 },
-    { 'id': 4, 'name': 'Small', 'hitMultiplier': 0.75 },
-    { 'id': 5, 'name': 'Medium', 'hitMultiplier': 1.00 },
-    { 'id': 6, 'name': 'Big', 'hitMultiplier': 1.50 },
-    { 'id': 7, 'name': 'Large', 'hitMultiplier': 2.00 },
-    { 'id': 8, 'name': 'Huge', 'hitMultiplier': 3.00 },
-    { 'id': 9, 'name': 'Gigantic', 'hitMultiplier': 4.00 },
-    { 'id': 10, 'name': 'Enormous', 'hitMultiplier': 5.00 },
+    { 'id': 'minuscule', 'index': 1, 'name': 'Minuscule', 'hitMultiplier': 0.25 },
+    { 'id': 'diminutive', 'index': 2, 'name': 'Diminutive', 'hitMultiplier': 0.50 },
+    { 'id': 'tiny', 'index': 3, 'name': 'Tiny', 'hitMultiplier': 0.67 },
+    { 'id': 'small', 'index': 4, 'name': 'Small', 'hitMultiplier': 0.75 },
+    { 'id': 'medium', 'index': 5, 'name': 'Medium', 'hitMultiplier': 1.00 },
+    { 'id': 'big', 'index': 6, 'name': 'Big', 'hitMultiplier': 1.50 },
+    { 'id': 'large', 'index': 7, 'name': 'Large', 'hitMultiplier': 2.00 },
+    { 'id': 'huge', 'index': 8, 'name': 'Huge', 'hitMultiplier': 3.00 },
+    { 'id': 'gigantic', 'index': 9, 'name': 'Gigantic', 'hitMultiplier': 4.00 },
+    { 'id': 'enormous', 'index': 10, 'name': 'Enormous', 'hitMultiplier': 5.00 },
 ];
 
 router.get('/', async (req, res) => {
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-    const id = parseInt(req.params.id);
+    const id = req.params.id;
     const item = characterSizes.find(item => item.id === id);
     if (item) {
         res.json(item);
