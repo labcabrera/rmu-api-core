fd9b930161cb9e1c101068b24202a5822e5a55c2
labcabrera <lab.cabrera@gmail.com>
2024-09-24 15:43:40 +0200

    Initial memory pagination.

--- src/routes/skill-category-controller.js
+++ src/routes/skill-category-controller.js
@@ -2,34 +2,6 @@ const express = require('express');
 const router = express.Router();
 const skillCategoryService = require("../services/skill-category-service");
 
-const categories = [
-    { 'id': "animal", bonus: ["ag", "em"] },
-    { 'id': "awareness", bonus: ["in", "re"] },
-    { 'id': "battle-expertise", bonus: [] },
-    { 'id': "body-discipline", bonus: ["co", "sd"] },
-    { 'id': "brawn", bonus: ["co", "sd"] },
-    { 'id': "combat-expertise", bonus: [] },
-    { 'id': "combat-training", bonus: ["ag", "st"] },
-    { 'id': "composition" },
-    { 'id': "crafting" },
-    { 'id': "delving" },
-    { 'id': "environmental" },
-    { 'id': "gymnastic" },
-    { 'id': "lore" },
-    { 'id': "magical-expertise" },
-    { 'id': "medical" },
-    { 'id': "mental-discipline" },
-    { 'id': "movement" },
-    { 'id': "performance-art" },
-    { 'id': "power-manipulation" },
-    { 'id': "science" },
-    { 'id': "social" },
-    { 'id': "spellcasting" },
-    { 'id': "subterfuge" },
-    { 'id': "techical" },
-    { 'id': "vocation" }
-];
-
 router.get('/', async (req, res) => {
     try {
         const page = req.query.page ? parseInt(req.query.page) : 0;
--- src/routes/skill-controller.js
+++ src/routes/skill-controller.js
@@ -1,154 +1,25 @@
 const express = require('express');
 const router = express.Router();
-
-const skills = [
-    { 'id': "animal-handling", categoryId: "animal", bonus: ["pr"], specializations: ["animal-type"] },
-    { 'id': "riding", categoryId: "animal", bonus: ["pr"], specializations: ["animal-type"] },
-
-    { 'id': "perception", categoryId: "awarenes", bonus: ["sd"], specializations: null },
-    { 'id': "tracking", categoryId: "awarenes", bonus: ["sd"], specializations: null },
-
-    { 'id': "armor-maneuver", categoryId: "battle-expertise", bonus: [], specializations: null },
-    { 'id': "mounted-combat", categoryId: "battle-expertise", bonus: [], specializations: null },
-    { 'id': "protect", categoryId: "battle-expertise", bonus: [], specializations: null },
-    { 'id': "restricted-quarters", categoryId: "battle-expertise", bonus: [], specializations: null },
-    { 'id': "subduing", categoryId: "battle-expertise", bonus: [], specializations: null },
-
-    { 'id': "adrenal-maneuvers", categoryId: "body-discipline", bonus: [], specializations: null },
-    { 'id': "adrenal-defense", categoryId: "body-discipline", bonus: ["ag"], specializations: null },
-    { 'id': "adrenal-focus", categoryId: "body-discipline", bonus: ["sd"], specializations: null },
-    { 'id': "adrenal-speed", categoryId: "body-discipline", bonus: ["qu"], specializations: null },
-    { 'id': "adrenal-strength", categoryId: "body-discipline", bonus: ["st"], specializations: null },
-
-    { 'id': "body-development", categoryId: "brawn", bonus: ["co"], specializations: null },
-    { 'id': "fortitude", categoryId: "brawn", bonus: ["sd"], specializations: null },
-    { 'id': "weight-training", categoryId: "brawn", bonus: ["st"], specializations: null },
-
-    { 'id': "blind-figthing", categoryId: "combat-expertise", bonus: [], specializations: null },
-    { 'id': "disarm", categoryId: "combat-expertise", bonus: [], specializations: null },
-    { 'id': "footwork", categoryId: "combat-expertise", bonus: [], specializations: null },
-    { 'id': "multiple-attacks", categoryId: "combat-expertise", bonus: [], specializations: null },
-    { 'id': "reverse-strike", categoryId: "combat-expertise", bonus: [], specializations: null },
-
-    { 'id': "melee-weapon:blade", categoryId: "combat-training", bonus: ["st"], specializations: null },
-    { 'id': "melee-weapon:chain", categoryId: "combat-training", bonus: ["st"], specializations: null },
-    { 'id': "melee-weapon:hafted", categoryId: "combat-training", bonus: ["st"], specializations: null },
-    { 'id': "melee-weapon:greater-blade", categoryId: "combat-training", bonus: ["st"], specializations: null },
-    { 'id': "melee-weapon:greater-chain", categoryId: "combat-training", bonus: ["st"], specializations: null },
-    { 'id': "melee-weapon:greater-hafted", categoryId: "combat-training", bonus: ["st"], specializations: null },
-    { 'id': "melee-weapon:pole-arm", categoryId: "combat-training", bonus: ["st"], specializations: null },
-    { 'id': "melee-weapon:exotic", categoryId: "combat-training", bonus: ["st"], specializations: null },
-
-    { 'id': "ranged-weapon:bow", categoryId: "combat-training", bonus: ["ag"], specializations: null },
-    { 'id': "ranged-weapon:crossbow", categoryId: "combat-training", bonus: ["ag"], specializations: null },
-    { 'id': "ranged-weapon:sling", categoryId: "combat-training", bonus: ["ag"], specializations: null },
-    { 'id': "ranged-weapon:thrown", categoryId: "combat-training", bonus: ["ag"], specializations: null },
-    { 'id': "ranged-weapon:exotic", categoryId: "combat-training", bonus: ["ag"], specializations: null },
-
-    { 'id': "shield", categoryId: "combat-training", bonus: ["st"], specializations: null },
-
-    { 'id': "ilusion-crafting", categoryId: "composition", bonus: ["pr"], specializations: null },
-    { 'id': "music-composition", categoryId: "composition", bonus: ["pr"], specializations: null },
-    { 'id': "writing", categoryId: "composition", bonus: ["re"], specializations: null },
-
-    { 'id': "culinary", categoryId: "crafting", bonus: ["sd"], specializations: null },
-    { 'id': "drawing-painting", categoryId: "crafting", bonus: ["in"], specializations: null },
-    { 'id': "fabric-craft", categoryId: "crafting", bonus: ["sd"], specializations: null },
-    { 'id': "leathercraft", categoryId: "crafting", bonus: ["sd"], specializations: null },
-    { 'id': "metalcraft", categoryId: "crafting", bonus: ["st"], specializations: null },
-    { 'id': "stonecraft", categoryId: "crafting", bonus: ["st"], specializations: null },
-    { 'id': "woodcraft", categoryId: "crafting", bonus: ["sd"], specializations: null },
-
-    { 'id': "attunement", categoryId: "delving", bonus: ["pr"], specializations: null },
-    { 'id': "runes", categoryId: "delving", bonus: ["pr"], specializations: null },
-
-    { 'id': "navigation", categoryId: "environmental", bonus: ["re"], specializations: null },
-    { 'id': "piloting", categoryId: "environmental", bonus: ["ag"], specializations: null },
-    { 'id': "survival", categoryId: "environmental", bonus: ["ag"], specializations: null },
-
-    { 'id': "acrobatics", categoryId: "gymnastic", bonus: ["st"], specializations: null },
-    { 'id': "contortions", categoryId: "gymnastic", bonus: ["sd"], specializations: null },
-    { 'id': "jumping", categoryId: "gymnastic", bonus: ["st"], specializations: null },
-
-    { 'id': "creature-lore", categoryId: "lore", bonus: ["re"], specializations: null },
-    { 'id': "historic-lore", categoryId: "lore", bonus: ["re"], specializations: null },
-    { 'id': "language", categoryId: "lore", bonus: ["re"], specializations: null },
-    { 'id': "materials-lore", categoryId: "lore", bonus: ["re"], specializations: null },
-    { 'id': "racial-lore", categoryId: "lore", bonus: ["re"], specializations: null },
-    { 'id': "region-lore", categoryId: "lore", bonus: ["re"], specializations: null },
-    { 'id': "religion-lore", categoryId: "lore", bonus: ["re"], specializations: null },
-    { 'id': "spell-lore", categoryId: "lore", bonus: ["re"], specializations: null },
-
-    { 'id': "herbalism", categoryId: "medical", bonus: ["re"], specializations: null },
-    { 'id': "medicine", categoryId: "medical", bonus: ["re"], specializations: null },
-    { 'id': "poison-mastery", categoryId: "medical", bonus: ["re"], specializations: null },
-
-    { 'id': "control-lycanthropy", categoryId: "medical", bonus: ["sd"], specializations: null },
-    { 'id': "meditation", categoryId: "medical", bonus: ["sd"], specializations: null },
-    { 'id': "mental-focus", categoryId: "medical", bonus: ["sd"], specializations: null },
-
-    { 'id': "climbing", categoryId: "movement", bonus: ["co"], specializations: null },
-    { 'id': "flying", categoryId: "movement", bonus: ["co"], specializations: null },
-    { 'id': "running", categoryId: "movement", bonus: ["co"], specializations: null },
-    { 'id': "swimming", categoryId: "movement", bonus: ["co"], specializations: null },
-
-    { 'id': "acting", categoryId: "performance-art", bonus: ["me"], specializations: null },
-    { 'id': "music", categoryId: "performance-art", bonus: ["me"], specializations: null },
-    { 'id': "state-magic", categoryId: "performance-art", bonus: ["ag"], specializations: null },
-
-    { 'id': "channeling", categoryId: "power-manipulation", bonus: ["sd"], specializations: null },
-    { 'id': "directed-spell", categoryId: "power-manipulation", bonus: ["ag"], specializations: null },
-    { 'id': "power-development", categoryId: "power-manipulation", bonus: ["co"], specializations: null },
-    { 'id': "power-projection", categoryId: "power-manipulation", bonus: ["sd"], specializations: null },
-
-    { 'id': "architecture", categoryId: "science", bonus: ["re"], specializations: null },
-    { 'id': "astronomy", categoryId: "science", bonus: ["re"], specializations: null },
-    { 'id': "engineering", categoryId: "science", bonus: ["re"], specializations: null },
-    { 'id': "mathematics", categoryId: "science", bonus: ["re"], specializations: null },
-
-    { 'id': "influence", categoryId: "social", bonus: ["pr"], specializations: null },
-    { 'id': "leadership", categoryId: "social", bonus: ["pr"], specializations: null },
-    { 'id': "social-awareness", categoryId: "social", bonus: ["em"], specializations: null },
-    { 'id': "trading", categoryId: "social", bonus: ["em"], specializations: null },
-
-    { 'id': "magic-ritual", categoryId: "spellcasting", bonus: ["me"], specializations: null },
-    { 'id': "base-spell-list", categoryId: "spellcasting", bonus: ["me"], specializations: null },
-    { 'id': "open-spell-list", categoryId: "spellcasting", bonus: ["me"], specializations: null },
-    { 'id': "closed-spell-list", categoryId: "spellcasting", bonus: ["me"], specializations: null },
-    { 'id': "arcane-spell-list", categoryId: "spellcasting", bonus: ["me"], specializations: null },
-    { 'id': "restricted-spell-list", categoryId: "spellcasting", bonus: ["me"], specializations: null },
-
-    { 'id': "ambus", categoryId: "subterfuge", bonus: ["in"], specializations: null },
-    { 'id': "concealment", categoryId: "subterfuge", bonus: ["in"], specializations: null },
-    { 'id': "stalking", categoryId: "subterfuge", bonus: ["in"], specializations: null },
-    { 'id': "trickery", categoryId: "subterfuge", bonus: ["in"], specializations: null },
-
-    { 'id': "locks", categoryId: "technical", bonus: ["ag"], specializations: null },
-    { 'id': "mechanics", categoryId: "technical", bonus: ["me"], specializations: null },
-    { 'id': "traps", categoryId: "technical", bonus: ["ag"], specializations: null },
-
-    { 'id': "administration", categoryId: "vocation", bonus: ["re"], specializations: null },
-    { 'id': "service", categoryId: "vocation", bonus: ["pr"], specializations: null },
-    { 'id': "trade", categoryId: "vocation", bonus: ["pr"], specializations: null },
-
-
-
-
-
-
-];
+const skillService = require("../services/skill-service");
 
 router.get('/', async (req, res) => {
-    res.json(skills);
+    try {
+        const page = req.query.page ? parseInt(req.query.page) : 0;
+        const size = req.query.size ? parseInt(req.query.size) : 10;
+        const categories = skillService.findAll(page, size);
+        res.json(categories);
+    } catch (error) {
+        res.status(error.status ? error.status : 500).json({ message: error.message });
+    }
 });
 
 router.get('/:id', async (req, res) => {
-    const id = req.params.id;
-    const item = skills.find(item => item.id === id);
-    if (item) {
-        res.json(item);
-    } else {
-        res.status(404).json({ message: 'Skill not found' });
+    try {
+        const id = req.params.id;
+        const skill = skillService.findById(id);
+        res.json(skill);
+    } catch (error) {
+        res.status(error.status ? error.status : 500).json({ message: error.message });
     }
 });
 
