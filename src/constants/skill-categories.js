e8d3908b1374a88e41d46fc79d5e10497220a463
labcabrera <lab.cabrera@gmail.com>
2024-10-01 11:56:45 +0200

    Updated skill categories.

---
+++ src/constants/skill-categories.js
@@ -0,0 +1,29 @@
+const skillCategories = [
+    { 'id': "animal", bonus: ["ag", "em"] },
+    { 'id': "awareness", bonus: ["in", "re"] },
+    { 'id': "battle-expertise", bonus: [] },
+    { 'id': "body-discipline", bonus: ["co", "sd"] },
+    { 'id': "brawn", bonus: ["co", "sd"] },
+    { 'id': "combat-expertise", bonus: [] },
+    { 'id': "combat-training", bonus: ["ag", "st"] },
+    { 'id': "composition" },
+    { 'id': "crafting" },
+    { 'id': "delving" },
+    { 'id': "environmental" },
+    { 'id': "gymnastic" },
+    { 'id': "lore" },
+    { 'id': "magical-expertise" },
+    { 'id': "medical" },
+    { 'id': "mental-discipline" },
+    { 'id': "movement" },
+    { 'id': "performance-art" },
+    { 'id': "power-manipulation" },
+    { 'id': "science" },
+    { 'id': "social" },
+    { 'id': "spellcasting" },
+    { 'id': "subterfuge" },
+    { 'id': "technical" },
+    { 'id': "vocation" }
+];
+
+module.exports = skillCategories;
--- src/services/skill-category-service.js
+++ src/services/skill-category-service.js
@@ -1,33 +1,7 @@
-const categories = [
-    { 'id': "animal", bonus: ["ag", "em"] },
-    { 'id': "awareness", bonus: ["in", "re"] },
-    { 'id': "battle-expertise", bonus: [] },
-    { 'id': "body-discipline", bonus: ["co", "sd"] },
-    { 'id': "brawn", bonus: ["co", "sd"] },
-    { 'id': "combat-expertise", bonus: [] },
-    { 'id': "combat-training", bonus: ["ag", "st"] },
-    { 'id': "composition" },
-    { 'id': "crafting" },
-    { 'id': "delving" },
-    { 'id': "environmental" },
-    { 'id': "gymnastic" },
-    { 'id': "lore" },
-    { 'id': "magical-expertise" },
-    { 'id': "medical" },
-    { 'id': "mental-discipline" },
-    { 'id': "movement" },
-    { 'id': "performance-art" },
-    { 'id': "power-manipulation" },
-    { 'id': "science" },
-    { 'id': "social" },
-    { 'id': "spellcasting" },
-    { 'id': "subterfuge" },
-    { 'id': "techical" },
-    { 'id': "vocation" }
-];
+const skillCategories = require('../constants/skill-categories');
 
 const findById = (id) => {
-    const item = categories.find(item => item.id === id);
+    const item = skillCategories.find(item => item.id === id);
     if (!item) {
         throw { status: 404, message: 'Skill category not found' }
     }
@@ -35,8 +9,8 @@ const findById = (id) => {
 }
 
 const findAll = (page, size) => {
-    const content = categories.slice(page * size, (page + 1) * size);
-    return { content: content, pagination: { page: page, size: size, totalElements: categories.length } };
+    const content = skillCategories.slice(page * size, (page + 1) * size);
+    return { content: content, pagination: { page: page, size: size, totalElements: skillCategories.length } };
 };
 
 module.exports = {
