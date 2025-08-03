60fb05484db081fb997cfac41c7dccd6a75ba2f6
labcabrera <lab.cabrera@gmail.com>
2025-08-03 16:15:18 +0200

    Moved constants to entity definitions.

--- src/infrastructure/adapters/outbound/persistence/repositories/in-memory-skill.repository.ts
+++ src/infrastructure/adapters/outbound/persistence/repositories/in-memory-skill.repository.ts
@@ -1,21 +1,19 @@
 import { injectable } from 'inversify';
 import { SkillRepository } from '@domain/ports/skill-repository';
-import { Skill } from '@domain/entities/skill';
-import { RMU_SKILLS } from '@shared/constants/skills';
+import { RMU_SKILLS, Skill } from '@domain/entities/skill';
 import { Page } from '@domain/entities/page';
 import { SkillQuery } from '@domain/queries/skill-query';
 
 @injectable()
 export class InMemorySkillRepository implements SkillRepository {
-  private skills: Skill[] = [...RMU_SKILLS];
 
   async findById(id: string): Promise<Skill | null> {
-    const skill = this.skills.find(skill => skill.id === id);
+    const skill = RMU_SKILLS.find(skill => skill.id === id);
     return skill || null;
   }
 
   async find(query: SkillQuery, page: number, size: number): Promise<Page<Skill>> {
-    const filteredSkills = this.skills.filter(skill => {
+    const filteredSkills = RMU_SKILLS.filter(skill => {
       if (query.id && skill.id !== query.id) {
         return false;
       }
