cd213b218f4df8cbf048de5e743f054c7f89978a
labcabrera <lab.cabrera@gmail.com>
2026-03-13 23:23:44 +0100

    Entity access security refactor.

--- src/modules/professions/application/cqrs/commands/create-profession.command.ts
+++ src/modules/professions/application/cqrs/commands/create-profession.command.ts
@@ -1,8 +1,9 @@
 import { ProfessionArchetype } from 'src/modules/professions/domain/value-objects/profession-archetype.vo';
 import { ProfessionSkillCosts } from 'src/modules/professions/domain/value-objects/profession-skill-cost.vo';
 import { RealmType } from 'src/modules/professions/domain/value-objects/realm-type.vo';
+import { AuthenticatedCommand } from 'src/modules/shared/application/cqrs/authenticated-command';
 
-export class CreateProfessionCommand {
+export class CreateProfessionCommand extends AuthenticatedCommand {
   constructor(
     public readonly id: string,
     public readonly archetype: ProfessionArchetype,
@@ -12,7 +13,9 @@ export class CreateProfessionCommand {
     public readonly professionalSkills: string[],
     public readonly description: string | undefined,
     public readonly imageUrl: string | undefined,
-    public readonly userId: string,
-    public readonly roles: string[],
-  ) {}
+    userId: string,
+    roles: string[],
+  ) {
+    super(userId, roles);
+  }
 }
--- src/modules/professions/application/cqrs/commands/update-profession.command.ts
+++ src/modules/professions/application/cqrs/commands/update-profession.command.ts
@@ -1,8 +1,9 @@
 import { ProfessionArchetype } from 'src/modules/professions/domain/value-objects/profession-archetype.vo';
 import { ProfessionSkillCosts } from 'src/modules/professions/domain/value-objects/profession-skill-cost.vo';
 import { RealmType } from 'src/modules/professions/domain/value-objects/realm-type.vo';
+import { AuthenticatedCommand } from 'src/modules/shared/application/cqrs/authenticated-command';
 
-export class UpdateProfessionCommand {
+export class UpdateProfessionCommand extends AuthenticatedCommand {
   constructor(
     public readonly id: string,
     public readonly archetype: ProfessionArchetype | undefined,
@@ -12,7 +13,9 @@ export class UpdateProfessionCommand {
     public readonly professionalSkills: string[] | undefined,
     public readonly description: string | undefined,
     public readonly imageUrl: string | undefined,
-    public readonly userId: string,
-    public readonly roles: string[],
-  ) {}
+    userId: string,
+    roles: string[],
+  ) {
+    super(userId, roles);
+  }
 }
