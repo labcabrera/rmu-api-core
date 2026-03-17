08baf0f54f53c4785f61e558da54d324f0d35c13
labcabrera <lab.cabrera@gmail.com>
2026-03-17 17:02:25 +0100

    Updated open api annotations.

--- src/modules/skills/interfaces/http/skill.controller.ts
+++ src/modules/skills/interfaces/http/skill.controller.ts
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Controller, Get, Param, Query, UseGuards, Post, Body, Request } from '@nestjs/common';
-import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/modules/auth/jwt.auth.guard';
 import { SkillDto, SkillPageDto } from './dto/skill.dto';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
@@ -10,6 +11,7 @@ import { GetSkillsQuery } from '../../application/cqrs/queries/get-skills.query'
 import { Page } from 'src/modules/shared/domain/entities/page';
 import { Skill } from '../../domain/aggregates/skill';
 import { NotFoundError } from 'src/modules/shared/domain/errors/errors';
+import { ErrorDto } from 'src/modules/shared/interfaces/http/dto/error-dto';
 
 @UseGuards(JwtAuthGuard)
 @Controller('v1/skills')
@@ -22,7 +24,9 @@ export class SkillController {
 
   @Get(':id')
   @ApiOperation({ operationId: 'findSkillById', summary: 'Find skill by id' })
-  @ApiOkResponse({ type: SkillDto })
+  @ApiOkResponse({ type: SkillDto, description: 'Success' })
+  @ApiUnauthorizedResponse({ description: 'Invalid or missing authentication token', type: ErrorDto })
+  @ApiNotFoundResponse({ description: 'Realm not found', type: ErrorDto })
   async findById(@Param('id') id: string, @Request() req) {
     const userId: string = req.user!.id as string;
     const roles: string[] = req.user!.roles as string[];
@@ -33,8 +37,10 @@ export class SkillController {
   }
 
   @Get('')
-  @ApiOperation({ operationId: 'findAllSkills', summary: 'Find all skills' })
-  @ApiOkResponse({ type: SkillPageDto })
+  @ApiOperation({ operationId: 'findSkills', summary: 'Find skills by RSQL' })
+  @ApiOkResponse({ type: SkillPageDto, description: 'Success' })
+  @ApiUnauthorizedResponse({ description: 'Invalid or missing authentication token', type: ErrorDto })
+  @ApiResponse({ status: 400, description: 'Invalid RSQL query', type: ErrorDto })
   async find(@Query() dto: PagedQueryDto, @Request() req): Promise<SkillPageDto> {
     const userId: string = req.user!.id as string;
     const roles: string[] = req.user!.roles as string[];
@@ -45,6 +51,11 @@ export class SkillController {
   }
 
   @Post('')
+  @ApiBody({ type: CreateSkillDto })
+  @ApiOperation({ operationId: 'findSkills', summary: 'Find skills by RSQL' })
+  @ApiOkResponse({ type: SkillDto, description: 'Success' })
+  @ApiResponse({ status: 400, description: 'Bad request, invalid data', type: ErrorDto })
+  @ApiResponse({ status: 409, description: 'Conflict, skill already exists', type: ErrorDto })
   create(@Body() createSkillDto: CreateSkillDto, @Request() req) {
     const userId: string = req.user!.id as string;
     const roles: string[] = req.user!.roles as string[];
